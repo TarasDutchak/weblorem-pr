f66bd14b78cc1b039d5244ce06b12e31bb752a09
TarasDutchak <Tarasdutchak90@gmail.com>
2024-02-01 17:32:36 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -74,24 +74,79 @@ $(document).ready(function () {
         spaceBetween: 30,
         breakpoints: {
             640: {
-              slidesPerView: 2,
-              spaceBetween: 20,
+                slidesPerView: 2,
+                spaceBetween: 20,
             },
-            768: {
-              slidesPerView: 4,
-              spaceBetween: 40,
+            1200: {
+                slidesPerView: 3.1,
+                spaceBetween: 30,
             },
             1500: {
-              slidesPerView: 3.3,
-              spaceBetween: 30,
+                slidesPerView: 3.3,
+                spaceBetween: 30,
             },
-          },
+        },
     });
 
     // language
-    $('.current-lang').click(function(){
+    $('.current-lang').click(function () {
         $('.language-list').slideToggle();
     });
 
+
+    // letters
+    // orderconsultation
+    
+
+  
+
+
 });
 
+var app = {
+    // chars: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    chars: ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+', '[', ']', '{', '}', '|', '\\', ';', ':', '\'', '"', ',', '.', '/', '<', '>', '?', '~', '\\', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+
+    init: function () {
+        app.container = document.createElement('div');
+        app.container.className = 'animation-container';
+
+        // Отримуємо елемент з класом orderconsultation
+        var orderConsultationElements = document.querySelectorAll('.orderconsultation');
+
+        // Перевірка, чи знайдено елементи з класом orderconsultation
+        if (orderConsultationElements.length > 0) {
+            // Додаємо контейнер в кожен елемент з класом orderconsultation
+            orderConsultationElements.forEach(function (element) {
+                element.appendChild(app.container);
+            });
+        } else {
+            // Якщо елементи не знайдено, додаємо контейнер в кінець body
+            document.body.appendChild(app.container);
+        }
+
+        window.setInterval(app.add, 100);
+    },
+
+    add: function () {
+        var element = document.createElement('span');
+        app.container.appendChild(element);
+        app.animate(element);
+    },
+
+    animate: function (element) {
+        var character = app.chars[Math.floor(Math.random() * app.chars.length)];
+        var duration = Math.floor(Math.random() * 15) + 1;
+        var offset = Math.floor(Math.random() * (50 - duration * 2)) + 3;
+        var size = 10 + (15 - duration);
+        element.style.cssText = 'right:' + offset + 'vw; font-size:' + size + 'px;animation-duration:' + duration + 's';
+        element.innerHTML = character;
+        window.setTimeout(app.remove, duration * 1000, element);
+    },
+
+    remove: function (element) {
+        element.parentNode.removeChild(element);
+    },
+};
+
+document.addEventListener('DOMContentLoaded', app.init);
